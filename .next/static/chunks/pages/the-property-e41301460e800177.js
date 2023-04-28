7770cf9319394d0c7ac652cf3c2491e9113ffba2
Duncan Matthews <duncanmatthews15@gmail.com>
2023-04-27 19:41:14 -0700

    delete file

--- .next/static/chunks/pages/the-property-e41301460e800177.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[670],{110:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/the-property",function(){return u(3185)}])},3185:function(){}},function(n){n.O(0,[774,888,179],function(){return n(n.s=110)}),_N_E=n.O()}]);
