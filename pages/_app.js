801ddcdf25660edbe00f84210a2e4334cd3e14f7
Duncan Matthews <duncanmatthews15@gmail.com>
2023-05-12 01:19:17 -0700

    updated css fiels

--- pages/_app.js
+++ pages/_app.js
@@ -24,33 +24,33 @@ function MyApp({ Component, pageProps }) {
         {/*====== FontAwesome css ======*/}
         <link
           rel="stylesheet"
-          href="assets/fonts/fontawesome/css/all.min.css"
+          href="/assets/fonts/fontawesome/css/all.min.css"
         />
         {/*====== Flaticon css ======*/}
-        <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css" />
+        <link rel="/stylesheet" href="assets/fonts/flaticon/flaticon.css" />
         {/*====== Bootstrap css ======*/}
         <link
           rel="stylesheet"
-          href="assets/vendor/bootstrap/css/bootstrap.min.css"
+          href="/assets/vendor/bootstrap/css/bootstrap.min.css"
         />
         {/*====== magnific-popup css ======*/}
         <link
           rel="stylesheet"
-          href="assets/vendor/magnific-popup/dist/magnific-popup.css"
+          href="/assets/vendor/magnific-popup/dist/magnific-popup.css"
         />
         {/*====== Slick-popup css ======*/}
-        <link rel="stylesheet" href="assets/vendor/slick/slick.css" />
+        <link rel="stylesheet" href="/assets/vendor/slick/slick.css" />
         {/*====== Nice Select css ======*/}
         <link
           rel="stylesheet"
-          href="assets/vendor/nice-select/css/nice-select.css"
+          href="/assets/vendor/nice-select/css/nice-select.css"
         />
         {/*====== Animate css ======*/}
-        <link rel="stylesheet" href="assets/vendor/animate.css" />
+        <link rel="stylesheet" href="/assets/vendor/animate.css" />
         {/*====== Default css ======*/}
-        <link rel="stylesheet" href="assets/css/default.css" />
+        <link rel="stylesheet" href="/assets/css/default.css" />
         {/*====== Style css ======*/}
-        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="/assets/css/style.css" />
       </Head>
       {loader && <PreLoader />}
       <Component {...pageProps} />
